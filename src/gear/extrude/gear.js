aff75a633f75b33e896e3cd230f105aa576e88ca
fujita <fujita@nulab-inc.com>
2017-01-15 03:15:37 +0900

    歯車を計算で作るようにした

--- src/gear/extrude/gear.js
+++ src/gear/extrude/gear.js
@@ -42,18 +42,29 @@
    * @param num 歯数
    * @param options 追加オプション(position等)
    */
-  function Gear(m, num, options) {
-    var d = num * m; // 基準円直径
-    var a = Math.PI * 2 / num; // 歯ごとの角度
+  function Gear(m, z, options) {
+    var d = z * m; // 基準円直径
+    var a = Math.PI * 2 / z; // 歯ごとの角度
     var cr = d - m * 2.5; // Cylinderの半径
-    var tcr = cr * 0.9; // 歯の位置を決めるための半径
-    var tw = cr * Math.sin(a / 3) / 2; // 歯先の幅の半分
-    var th = d - cr * Math.cos(a / 3) + m * 2; // 歯の台形の高さ
+    // 歯の形
+    var t1 = {
+      x: cr * Math.cos(a / 3) * 0.9,
+      y: cr * Math.sin(a / 3) * 0.9
+    };
+    var t2 = {
+      x: d * Math.cos(a / 5),
+      y: d * Math.sin(a / 5)
+    };
+    var t3 = {
+      x: (d + 2*m) * Math.cos(a / 30),
+      y: (d + 2*m) * Math.sin(a / 30)
+    };
+
     function teeth(i) {
       var v2 = THREE.Vector2;
       var shape = new THREE.Shape([
-        new v2(0.6 * tw, th), new v2(1.5 * tw, 0.5 * th), new v2(tw * 2, 0),
-        new v2(-2 * tw, 0), new v2(-1.5 * tw, 0.5 * th), new v2(-0.6 * tw, th)
+        new v2(t1.x, t1.y), new v2(t2.x, t2.y), new v2(t3.x, t3.y),
+        new v2(t3.x, -t3.y), new v2(t2.x, -t2.y), new v2(t1.x, -t1.y)
       ]);
       return new WHS.Extrude({
         geometry: {
@@ -61,10 +72,11 @@
           options: {
             bevelEnabled: false,
             bevelSize: 0,
-            amount: 1
+            amount: 1.8
           }
         },
-        position: [tcr * Math.cos(a * i), 0.5, tcr * Math.sin(a * i)],
+        mass:0.01,
+        position: [0, 0.9, 0],
         rotation: [Math.PI / 2, 0, a * i - Math.PI / 2],
         material: material,
       });
@@ -73,17 +85,18 @@
       geometry: {
         radiusTop: cr,
         radiusBottom: cr,
-        height: 1
+        height: 2
       },
-      mass: 1,
+      mass: 0.01,
       material: material
     }, options));
-    for (var i = 0; i < num; i++) {
+    for (var i = 0; i < z; i++) {
       c.add(teeth(i));
     }
     this.shape = c;
     this.d = d;
     this.m = m;
+    this.z = z;
   }
 
   var App = {
@@ -108,7 +121,7 @@
         position: [0, 0, 0]
       });
       this.add(base);
-      var g1 = new Gear(0.5, 10);
+      var g1 = new Gear(0.2, 10);
       g1.shape.position.set(0, 1.5, 0);
       this.add(g1.shape);
       var c1 = new WHS.HingeConstraint(base, g1.shape,
@@ -117,7 +130,7 @@
       );
       this.addConstraint(c1);
 
-      var g2 = new Gear(0.5, 15);
+      var g2 = new Gear(0.2, 15);
       g2.shape.position.set(g1.d + g2.d, 1.5, 0);
       g2.shape.rotation.set(0, Math.PI / g2.m, 0);
       this.add(g2.shape);
@@ -125,24 +138,27 @@
         g2.shape.position,
         new THREE.Vector3(0, 1, 0)
       );
+      //c2.enableAngularMotor(-1, 10);
       this.addConstraint(c2);
 
 
-      var g3 = new Gear(0.5, 20);
-      g3.shape.position.set(g1.d + g2.d, 1.5, g2.d + g3.d - 0.25);
+      var g3 = new Gear(0.2, 50);
+      g3.shape.position.set(g1.d + g2.d, 1.5, g2.d + g3.d);
       g3.shape.rotation.set(0, Math.PI / g3.m, 0);
       this.add(g3.shape);
       var c3 = new WHS.HingeConstraint(base, g3.shape,
         g3.shape.position,
         new THREE.Vector3(0, 1, 0)
       );
+      //c3.enableAngularMotor(-1, 10);
       this.addConstraint(c3);
 
 
 
-      new WHS.Loop(function() {
-        g3.shape.rotation.y += 0.002;
-      }).start(world);
+      var l = new WHS.Loop(function() {
+        g3.shape.rotation.y += 0.001;
+      });
+      l.start(world);
 
 
 
